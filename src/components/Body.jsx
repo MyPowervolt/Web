a85cad5cdae49efe2c5ce7692289202e30925a70
MyPowervolt <web.powervolt.it>
2026-01-25 20:48:35 +0100

    Cambiato lo stato delle card

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,9 @@
+import { useState } from "react";
 import Card from "./CardsSection";
 import { cardsData } from "../data/cardsData";
 
 export default function Body() {
-
+  const [openId, setOpenId] = useState(null);
 
   return (
     <section className="w-[80%] mx-auto space-y-4 mt-10">
@@ -12,8 +13,11 @@ export default function Body() {
           title={card.title}
           description={card.description}
           features={card.features}
+          isOpen={openId === card.id}
+          onToggle={() => setOpenId((prev) => (prev === card.id ? null : card.id))}
         />
       ))}
     </section>
   );
 }
+
